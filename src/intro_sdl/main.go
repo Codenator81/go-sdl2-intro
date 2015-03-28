929011e71c0b82035ee4b46d608deb64ff0c57db
Codenator81 <apoltarjonoks@gmail.com>
2015-03-28 22:38:31 +0200

    Test sdl-2. It works!

--- src/intro_sdl/main.go
+++ src/intro_sdl/main.go
@@ -1,6 +1,27 @@
 package main
-import "fmt"
+import (
+    "github.com/veandco/go-sdl2/sdl"
+)
 
 func main() {
-    fmt.Println("Hi")
+    sdl.Init(sdl.INIT_EVERYTHING)
+
+    window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+        800, 600, sdl.WINDOW_SHOWN)
+    if err != nil {
+        panic(err)
+    }
+    defer window.Destroy()
+
+    surface, err := window.GetSurface()
+    if err != nil {
+        panic(err)
+    }
+
+    rect := sdl.Rect{0, 0, 200, 200}
+    surface.FillRect(&rect, 0xffff0000)
+    window.UpdateSurface()
+
+    sdl.Delay(1000)
+    sdl.Quit()
 }
