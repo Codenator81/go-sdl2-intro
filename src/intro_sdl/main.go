2f2357caf1a6a4732110d887e83c576df6f3709b
Codenator81 <apoltarjonoks@gmail.com>
2015-04-03 23:06:35 +0300

    Creating fixed frames per second

--- src/intro_sdl/main.go
+++ src/intro_sdl/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"github.com/veandco/go-sdl2/sdl"
-	"runtime"
+	//"runtime"
 )
+// Here we define how many frames per second we want our game to run at
+const FPS uint32 = 60
+const DELAY_TIME uint32 = uint32(1000.0 / FPS)
 
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
+	//runtime.GOMAXPROCS(runtime.NumCPU())
+	var frameStart, frameTime uint32
 	var game Game
 	tm := TextureManager{textureMap: map[string]*sdl.Texture{}}
 	// before last parameter bool fullscreen
@@ -15,10 +19,14 @@ func main() {
 	game.gameRunning = true
 	//Game loop
 	for game.gameRunning {
+		frameStart = sdl.GetTicks()
 		game.HandleEvents()
 		game.Update()
 		game.Render(&tm)
-		sdl.Delay(10)
+		frameTime = sdl.GetTicks() - frameStart
+		if frameTime < DELAY_TIME {
+			sdl.Delay(uint32(DELAY_TIME - frameTime))
+		}
 	}
 	game.Clean()
 }
